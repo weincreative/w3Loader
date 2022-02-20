883cf57625ddaaf5911326d85eb5c226ee522215
WEIN CREATIVE <2136736+weincreative@users.noreply.github.com>
2022-02-20 14:11:16 +0300

    Delete ven directory

--- ven/runner.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-python3 whenLambo.py -sx BLZ -lx 20 -bx 1 -sbx 2 -stx 500 &
-python3 whenLambo.py -sx GRT -lx 20 -bx 1 -sbx 2 -stx 500 &
-python3 whenLambo.py -sx RVN -lx 20 -bx 1 -sbx 2 -stx 500 &
-python3 whenLambo.py -sx QTUM -lx 20 -bx 1 -sbx 2 -stx 500 &
-python3 whenLambo.py -sx REEF -lx 20 -bx 1 -sbx 2 -stx 500 &
-python3 whenLambo.py -sx RUNE -lx 20 -bx 1 -sbx 2 -stx 500 &
-python3 whenLambo.py -sx SAND -lx 20 -bx 1 -sbx 2 -stx 500
